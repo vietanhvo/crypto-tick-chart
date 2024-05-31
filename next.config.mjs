97b035800102b2a896bdd6c2e7fdae05f74333b9
Viet Anh Vo <vietanhvo@pm.me>
2024-05-31 11:21:36 +0700

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    serverComponentsExternalPackages: ["@mt/next-infra"],
+  },
 };
 
 export default nextConfig;
